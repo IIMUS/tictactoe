29afddb92e2f30b4840ab0698baaeea57ff81f4c
IIMUS <33875911+IIMUS@users.noreply.github.com>
2017-11-21 15:20:36 -0500

    Add files via upload

---
+++ master.py
@@ -0,0 +1,15 @@
+'''
+master.py
+Emrick St-Pierre
+21/11/2017
+'''
+
+#C'est ici que le jeu de tic tac toe doit etre code
+
+print("Hello World!")
+
+'''
+Note: les commentaires ne doivent pas contenir de
+caracteres en dehors du code ASCII standard, donc
+pas d'accents
+'''
