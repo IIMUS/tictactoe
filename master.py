b5921296a9ffa9e3ac0f594ba487fee5a06fdbda
IIMUS <33875911+IIMUS@users.noreply.github.com>
2017-11-29 20:43:48 -0500

    intégration de mon tic tac toe avec le master.py

--- master.py
+++ master.py
@@ -4,9 +4,33 @@ Emrick St-Pierre
 21/11/2017
 '''
 
+from fonctions import input_clavier
+from partie import Partie
+
 #C'est ici que le jeu de tic tac toe doit etre code
 
-print("Hello World!")
+# definitions fonctions
+
+def loop_jouer(_nom_joueur1, _nom_joueur2):
+    while(True):
+        try:
+            c = input("Voulez-vous jouer une partie? Oui:1 Non:0 ")
+            if(c == 1):
+                Partie(_nom_joueur1, _nom_joueur2).jouer()
+                print "Merci d'avoir joue"
+            if(c == 0):
+                print "Au revoir"
+                break
+            if(c != 1 and c != 0):
+                print "Entree invalide: {0}".format(c)
+        except NameError as error:
+            print "---NAME_ERROR LORS DE L'INSERTION---:", error
+        except SyntaxError as error:
+            print "---SYNTAX_ERROR LORS DE L'INSERTION---:", error
+
+#main
+
+loop_jouer("joueur 1", "joueur 2")
 
 '''
 Note: les commentaires ne doivent pas contenir de
